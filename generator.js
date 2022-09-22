f497dbad0e910a1083abaa10c5097d7219a98dbf
Alena Nemtsova <jelenatbs@gmail.com>
2022-09-22 17:03:16 +0300

    combined two for-loops into one

--- generator.js
+++ generator.js
@@ -3,26 +3,31 @@ btn.addEventListener('click', generate);
 
 function generate() {
     const numbers = [];
+    let sum = 0;
+    let product = 1;
 
     for (let i = 0; i < 10; i++) {
         min = Math.ceil(-10);
         max = Math.floor(10);
         const num = Math.floor(Math.random() * (max - min + 1)) + min;
-
         numbers.push(num);
+
+        sum += numbers[i];
+        product *= numbers[i];
     }
 
     const minNumber = Math.min.apply(null, numbers);
     const maxNumber = Math.max.apply(null, numbers);
-
-    let sum = 0;
-    let product = 1;
-
-    for (let i = 0; i < 10; i++) {
-        sum += numbers[i];
-        product *= numbers[i];
-    }
     const meanNumber = sum / 10;
+
+    // let sum = 0;
+    // let product = 1;
+
+    // for (let i = 0; i < 10; i++) {
+    //     sum += numbers[i];
+    //     product *= numbers[i];
+    // }
+    // const meanNumber = sum / 10;
 
     document.querySelector('.all-numbers').textContent = numbers.join(' ');
     document.querySelector('.min-number').textContent = minNumber;
